509cdbd7d4d28ee7b8d3d5ab0942419fa4ec0e8b
Dan <dchen12812@gmail.com>
2015-10-28 21:12:46 -0700

    added todo item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029040454) do
+ActiveRecord::Schema.define(version: 20151029040815) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20151029040454) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "todo_items", force: :cascade do |t|
+    t.date     "due_date"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "todo_lists", force: :cascade do |t|
