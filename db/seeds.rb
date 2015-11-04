dd8fc70de4f425536e7ece20f682e90f1ee98863
Dan <dchen12812@gmail.com>
2015-11-03 23:46:56 -0800

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create!(username: 'Fiorina', password_digest: 'password')
-#User.first.profile.build(gender: 'female', birth_year: 1954, first_name: "Carly", last_name: "Fiorina")
 User.create!(username: 'Trump', password_digest: 'password')
 User.create!(username: 'Carson', password_digest: 'password')
 User.create!(username: 'Clinton', password_digest: 'password')
@@ -16,3 +15,9 @@ Profile.create!(gender: 'female', birth_year: 1954, first_name: "Carly", last_na
 Profile.create!(gender: 'male', birth_year: 1946, first_name: "Donald", last_name: "Trump")
 Profile.create!(gender: 'male', birth_year: 1951, first_name: "Ben", last_name: "Carson")
 Profile.create!(gender: 'female', birth_year: 1947, first_name: "Hillary", last_name: "Clinton")
+
+TodoList.create!(list_name:"Cat", list_due_date: 1.year)
+
+(1...5).each do |i|
+	TodoItem.create!(due_date: 1.year, title: "Dog #{i}", description: "Catdog")
+end
