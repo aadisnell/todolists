9a18af2c500095cd500127e389dca800ac97a1f4
Dan <dchen12812@gmail.com>
2015-11-03 22:58:23 -0800

    updated tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029043122) do
+ActiveRecord::Schema.define(version: 20151104065525) do
 
   create_table "assignments", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20151029043122) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "completed"
   end
 
   create_table "todo_lists", force: :cascade do |t|
