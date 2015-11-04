91f856c6b0aa68298e1d4a93628b9c31faacf905
Dan <dchen12812@gmail.com>
2015-11-04 02:03:19 -0800

    updated validation for todo_item

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user
-	validates :first_name, :last_name, presence: true
+	validates_presence_of :first_name, :last_name
+	validates_inclusion_of :gender, :in => ["male", "female"]
 end
