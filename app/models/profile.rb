abd2ffd239d80c393a118eae4170509447bade45
Dan <dchen12812@gmail.com>
2015-11-04 02:38:23 -0800

    exclusion for sue

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	validates_presence_of :first_name, :last_name
 	validates_inclusion_of :gender, :in => ["male", "female"]
+	validates :first_name, exclusion: ["Sue"], if: "gender == 'male'"
 end
