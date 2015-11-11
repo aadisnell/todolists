29dccb8bd5ef275d6c64e53c6cd7dc30822d212d
Dan <dchen12812@gmail.com>
2015-11-10 21:14:41 -0800

    added new spec files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,11 @@
-#Reinit seed
-User.destroy_all
-TodoList.destroy_all
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+
 TodoItem.destroy_all
-Profile.destroy_all
 
-Profile.create!([
-				 {gender: 'female', birth_year: 1954, first_name: "Carly",   last_name: "Fiorina"},
-				 {gender: 'male',   birth_year: 1946, first_name: "Donald",  last_name: "Trump"},
-				 {gender: 'male',   birth_year: 1951, first_name: "Ben",     last_name: "Carson"},
-				 {gender: 'female', birth_year: 1947, first_name: "Hillary", last_name: "Clinton"}])
-
-profiles = Profile.all
-due_date = Date.today + 1.year
-
-profiles.each do |profile|
-	profile.create_user(username: 		  profile.last_name,
-						password_digest: 'password' )
-	profile.user.todo_lists.create!(list_name: "#{profile.user.username}'s list",
-									list_due_date: due_date)
-	5.times do
-		profile.user.todo_lists.last.todo_items.create!(due_date:    due_date,
-													    title:       "Cat",
-													    description: "Dog")
-	end
-end
-
+TodoItem.create! [
+  { title: "Task 1", due_date: today, description: "very important task TEST", completed: false },
+  { title: "Task 2", due_date: two_days_ago, description: "learn ruby TEST", completed: true},
+  { title: "Task 3", due_date: three_days_ago, description: "learn Active Record TEST", completed: true}
+]
