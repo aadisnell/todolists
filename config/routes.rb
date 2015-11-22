a8642d854d80e537d0a8e1ac5666edc662c845bd
Dan <dchen12812@gmail.com>
2015-11-21 22:33:18 -0800

    aded sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 
   root 'todo_lists#index'
   resources :todo_lists do
